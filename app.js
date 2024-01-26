ae46b232fb9206357693fb43fcc69e2e63c769a8
Bewizi <toluwalase.bewaji@yahoo.com>
2024-01-26 17:15:49 +0100

    bewizi

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
 const nav = document.querySelector("nav");
 const openNav = document.querySelector(".menu");
 const closeNav = document.querySelector(".close");
+const links = document.querySelector(".links");
+const listOfLInks = document.querySelectorAll(".list-links");
 
 // this is a click funtion for opennav and closenav
 openNav.addEventListener("click", () => {
   openNav.style.display = "none";
   closeNav.style.display = "block";
   nav.style.display = "block";
+  if (listOfLInks) {
+    listOfLInks.forEach((link) => {
+      link.addEventListener("click", () => {
+        nav.style.display = "none";
+        openNav.style.display = "block";
+        closeNav.style.display = "none";
+      });
+    });
+  }
 });
 closeNav.addEventListener("click", () => {
   openNav.style.display = "block";
