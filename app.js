9d4156ba50e480bea2182f4e0202ed1765583bce
Bewizi <toluwalase.bewaji@yahoo.com>
2024-01-26 16:27:00 +0100

    bewizi

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const nav = document.querySelector("nav");
 const openNav = document.querySelector(".menu");
 const closeNav = document.querySelector(".close");
+const listOfLInks = document.querySelectorAll(".list-links");
 
+// this is a click funtion for opennav and closenav
 openNav.addEventListener("click", () => {
   openNav.style.display = "none";
   closeNav.style.display = "block";
@@ -12,3 +14,14 @@ closeNav.addEventListener("click", () => {
   closeNav.style.display = "none";
   nav.style.display = "none";
 });
+
+// when the list are clicked the nav should hide
+if (listOfLInks && nav) {
+  listOfLInks.forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.style.display = "none";
+      openNav.style.display = "block";
+      closeNav.style.display = "none";
+    });
+  });
+}
